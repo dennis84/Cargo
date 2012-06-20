6d9708477c8dbede6df122a00d27dfd40a202a14
Dennis Dietrich <d.dietrich84@googlemail.com>
2012-06-20 16:47:29 +0200

    created an example

---
+++ sample/web/index.php
@@ -0,0 +1,22 @@
+<?php
+
+ini_set('display_errors', 1);
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+$app = new \Silex\Application();
+
+$app->register(new \Silex\Provider\TwigServiceProvider());
+
+$app->register(new \Cargo\Provider\CargoServiceProvider(), array(
+    'cargo.cache_dir' => __DIR__ . '/../cache',
+));
+
+$app['cargo']->registerThemes(array(
+    'website' => __DIR__.'/../themes/website',
+    'wiki'    => __DIR__.'/../themes/wiki',
+));
+
+$app['debug'] = true;
+
+$app->run();
